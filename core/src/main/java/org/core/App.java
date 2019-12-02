c1a118a02809aeafccb134c56312428210c1d81a
Pradeep <Pradeep@Lenovo>
2019-12-02 19:07:47 +0530

    added extra line at end.

--- core/src/main/java/org/core/App.java
+++ core/src/main/java/org/core/App.java
@@ -6,16 +6,16 @@ package org.core;
  */
 public class App 
 {
-<<<<<<< HEAD
 	private void function1222() {
 		System.out.println("Pradeep");
-=======
+	}
 	private void function123() {
 		System.out.println("Anjan");
->>>>>>> branch 'fuute/AEM-2020_addition' of https://github.com/pradeepkumar1246/master.git
 	}
     public static void main( String[] args )
     {
         System.out.println( "Hello World1234!" );
+        System.out.println("sdfsdf");
     }
+    
 }
